35bff69f4b129de16f1f63b71e8620c48c116185
timriffe <tim.riffe@gmail.com>
2026-03-11 19:58:32 +0100

    .

--- R/world_fig.R
+++ R/world_fig.R
@@ -25,36 +25,17 @@ dec |>
   facet_grid(vars(world1),vars(world2))+
   theme_minimal()
 
-# install.packages("ggh4x")
-library(ggh4x)
-dec |> 
+dec_fig_margins <-
+  dec |>
   mutate(world_hold = world1,
          world1 = world2,
          world2 = world_hold,
-         cc = -cc) |> 
-  select(-world_hold) |> 
-  bind_rows(dec) |> 
-  filter(system == "returns") |> 
-  group_by(world1,world2,trans) |> 
-  ggplot(aes(x = trans, y = cc, fill = trans)) +
-  geom_rect(data = data.frame(x = 0, y = 0,trans=NA,cc=0),aes(xmin=0,xmax=4.5,ymin=-Inf,ymax=0),
-            fill="#3287a820")+
-  geom_rect(data = data.frame(x = 0, y = 0,trans=NA,cc=0),aes(xmin=0,xmax=4.5,ymin=0,ymax=Inf),
-            fill="#a8324020")+
-  geom_col() +
-  scale_y_continuous(breaks = c(-3,0,3))+
-  facet_grid(vars(world1),vars(world2))+
-  theme_minimal() +
-  theme(panel.grid.major.x = element_blank(),
-        panel.grid.major.y = element_blank(),
-        panel.grid.minor.x = element_blank(),
-        panel.grid.minor.y = element_blank(),
-        axis.text.x = element_blank(),
-        legend.position="bottom"
-        ) +
-  labs(x="world 2", y = "world 1",fill="hazard:")
-library(dplyr)
-library(ggplot2)
+         cc = -cc) |>
+  select(-world_hold) |>
+  bind_rows(dec)  |> 
+  filter(system == "returns") |>
+  summarize(cc = sum(cc),
+            .by = c(world1, world2, trans))
 
 tick_dat <- data.frame(
   world1 = 1,
@@ -89,25 +70,43 @@ bg_top <- bg_dat |>
   mutate(ymin = 0, ymax = Inf,
          xmin = 0, xmax = 4.5,
          fill_col = "#a8324020")
-dec |>
-  mutate(world_hold = world1,
-         world1 = world2,
-         world2 = world_hold,
-         cc = -cc) |>
-  select(-world_hold) |>
-  bind_rows(dec) |>
-  filter(system == "returns") |>
-  group_by(world1, world2, trans) |>
+bg_panels <- dec_fig_margins |>
+  distinct(world1, world2) |>
+  full_join(
+    expand_grid(
+      world1 = sort(unique(dec_fig_margins$world1)),
+      world2 = sort(unique(dec_fig_margins$world2))
+    ),
+    by = c("world1", "world2")
+  ) |>
+  distinct(world1, world2) |>
+  filter(world1 != world2 | (world1 == 1 & world2 == 1))
+
+bg_neg <- bg_panels |>
+  mutate(xmin = 0, xmax = 4.5, ymin = -Inf, ymax = 0)
+
+bg_pos <- bg_panels |>
+  mutate(xmin = 0, xmax = 4.5, ymin = 0, ymax = Inf)
+
+p<-
+dec_fig_margins |>
   ggplot(aes(x = trans, y = cc, fill = trans)) +
   
-  annotate("rect", xmin = 0, xmax = 4.5, ymin = -Inf, ymax = 0,
-           fill = "#3287a820") +
-  annotate("rect", xmin = 0, xmax = 4.5, ymin = 0, ymax = Inf,
-           fill = "#a8324020") +
+  geom_rect(
+    data = bg_neg,
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+    inherit.aes = FALSE,
+    fill = "#3287a820"
+  ) +
+  geom_rect(
+    data = bg_pos,
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+    inherit.aes = FALSE,
+    fill = "#a8324020"
+  ) +
   
   geom_col() +
   
-  # fake y-axis only in upper-left facet
   geom_segment(
     data = axis_seg,
     aes(x = x, xend = x, y = -3.1, yend = 3.1),
@@ -127,7 +126,7 @@ dec |>
   ) +
   
   scale_y_continuous(breaks = NULL) +
-  facet_grid(vars(world1), vars(world2),switch = "both") +
+  facet_grid(vars(world1), vars(world2), switch = "both") +
   coord_cartesian(clip = "off") +
   theme_minimal() +
   theme(
@@ -138,6 +137,160 @@ dec |>
     legend.position = "bottom",
     plot.margin = margin(5.5, 5.5, 5.5, 22),
     strip.placement = "outside",
-    strip.background = element_blank()
+    strip.background = element_blank(),
+    strip.text.y.left = element_text(angle = 0),
+    strip.text.x.bottom = element_text(angle = 0)
   ) +
   labs(x = "world 2", y = "world 1", fill = "hazard:")
+ggsave(p,file="figs/dec_returns_margins.pdf",width=30,height=30,units="cm")
+# --------------------------------------
+# age profile panel:
+dec_all <-
+  dec |>
+  mutate(world_hold = world1,
+         world1 = world2,
+         world2 = world_hold,
+         cc = -cc) |>
+  select(-world_hold) |>
+  bind_rows(dec) 
+
+
+bg_panels <- expand_grid(
+  world1 = sort(unique(dec_all$world1)),
+  world2 = sort(unique(dec_all$world2))
+) |>
+  filter(world1 != world2 | (world1 == 1 & world2 == 1))
+
+bg_neg <- bg_panels |>
+  mutate(xmin = 50, xmax = 100, ymin = -0.1, ymax = 0)
+
+bg_pos <- bg_panels |>
+  mutate(xmin = 50, xmax = 100, ymin = 0, ymax = 0.1)
+
+# fake y-axis for upper-left facet
+tick_dat_y <- data.frame(
+  world1 = 1,
+  world2 = 1,
+  y = c(-0.1, 0, 0.1),
+  lab = c("-0.1", "0", "0.1")
+)
+
+tick_seg_y <- data.frame(
+  world1 = 1,
+  world2 = 1,
+  y = c(-0.1, 0, 0.1)
+)
+
+axis_seg_y <- data.frame(
+  world1 = 1,
+  world2 = 1,
+  x = 50
+)
+
+# fake x-axis for upper-left facet
+tick_dat_x <- data.frame(
+  world1 = 1,
+  world2 = 1,
+  x = c(50, 75, 100),
+  lab = c("50", "Age", "100")
+)
+
+tick_seg_x <- data.frame(
+  world1 = 1,
+  world2 = 1,
+  x = c(50, 100),
+  y0 = c(-0.1, -0.1),
+  y1 = c(-0.106, -0.05)
+)
+
+axis_seg_x <- data.frame(
+  world1 = 1,
+  world2 = 1,
+  y = -0.1
+)
+
+p<-
+dec_all |>
+  filter(system=="returns") |> 
+  ggplot(aes(x = age, y = cc, color = trans)) +
+  
+  geom_rect(
+    data = bg_neg,
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+    inherit.aes = FALSE,
+    fill = "#3287a820"
+  ) +
+  geom_rect(
+    data = bg_pos,
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax),
+    inherit.aes = FALSE,
+    fill = "#a8324020"
+  ) +
+  
+  geom_line() +
+  
+  # fake y-axis only in upper-left facet
+  geom_segment(
+    data = axis_seg_y,
+    aes(x = x, xend = x, y = -0.1, yend = 0.1),
+    inherit.aes = FALSE
+  ) +
+  geom_segment(
+    data = tick_seg_y,
+    aes(x = 49.2, xend = 50, y = y, yend = y),
+    inherit.aes = FALSE
+  ) +
+  geom_text(
+    data = tick_dat_y,
+    aes(x = 48.9, y = y, label = lab),
+    inherit.aes = FALSE,
+    hjust = 1,
+    size = 2.8
+  ) +
+  
+  # fake x-axis only in upper-left facet
+  geom_segment(
+    data = tick_seg_x,
+    aes(x = x, xend = x, y = y0, yend = y1),
+    inherit.aes = FALSE
+  ) +
+  geom_text(
+    data = tick_dat_x,
+    aes(x = x, y = -0.112, label = lab),
+    inherit.aes = FALSE,
+    vjust = 1,
+    size = 2.8
+  ) +
+  
+  scale_x_continuous(
+    breaks = NULL,
+    expand = expansion(mult = 0)
+  ) +
+  scale_y_continuous(
+    breaks = NULL,
+    expand = expansion(mult = 0)
+  ) +
+  facet_grid(vars(world1), vars(world2), switch = "both") +
+  coord_cartesian(
+    xlim = c(48, 100),
+    ylim = c(-0.12, 0.1),
+    clip = "off"
+  ) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),
+    axis.text.x = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.ticks.y = element_blank(),
+    legend.position = "bottom",
+    plot.margin = margin(12, 5.5, 18, 30),
+    strip.placement = "outside",
+    strip.background = element_blank(),
+    
+    strip.text.y.left = element_text(angle = 0),
+    strip.text.x.bottom = element_text(angle = 0)
+  ) +
+  labs(x = "world 2", y = "world 1", color = "hazard:")
+p
+ggsave(p,file="figs/dec_returns_age.pdf",width=30,height=30,units="cm")
